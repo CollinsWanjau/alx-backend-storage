e5a2fd08767609059719c860c7b35582daa0f4b6
colloso999 <tripperskripper@gmail.com>
2023-10-14 18:07:18 +0300

    enumeration of countries

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,5 @@
+-- An enumeration is a data type that allows you to define a list of 
+-- possible values for a column.
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
