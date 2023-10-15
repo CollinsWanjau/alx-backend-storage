5bd49eee99f0b77ea7d7060c58fd5a18011bd949
colloso999 <tripperskripper@gmail.com>
2023-10-15 10:19:53 +0300

    Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,8 @@
+-- we rank the country origins of bands by number of fans using the SELECT 
+-- statement. We group the results by the origin column and count the 
+-- number of bands for each origin using the SUM(*).
+-- We then order the results by the number of fans in descending 
+-- order using the ORDER BY clause.
 SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
